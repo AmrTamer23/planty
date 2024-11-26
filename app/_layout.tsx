b74fd43a9423c495cea1432861be9a00393c05ac
AmrTamer23 <amrtamer23mj@gmail.com>
2024-11-26 22:30:20 +0200

    chore: give up on gluestick ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,12 @@
 import { Stack } from "expo-router";
 
 import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider mode="dark">
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="onboarding" options={{ title:"Welcome to Planty" }} />
-      </Stack>
-    </GluestackUIProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+    </Stack>
   );
 }
